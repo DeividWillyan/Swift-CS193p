920c656d3a856baa2681126f858a26e8a10909f9
Deivid Willyan <deivid.fabiano@invillia.com>
2020-12-30 11:00:46 -0300

    ViewBuilder, Shape e ViewModifier

--- Memorize/GridLayout.swift
+++ Memorize/GridLayout.swift
@@ -9,19 +9,13 @@
 import SwiftUI
 
 struct GridLayout {
-    var size: CGSize
-    var rowCount: Int = 0
-    var columnCount: Int = 0
+    private(set) var size: CGSize
+    private(set) var rowCount: Int = 0
+    private(set) var columnCount: Int = 0
     
     init(itemCount: Int, nearAspectRatio desiredAspectRatio: Double = 1, in size: CGSize) {
         self.size = size
-        // if our size is zero width or height or the itemCount is not > 0
-        // then we have no work to do (because our rowCount & columnCount will be zero)
         guard size.width != 0, size.height != 0, itemCount > 0 else { return }
-        // find the bestLayout
-        // i.e., one which results in cells whose aspectRatio
-        // has the smallestVariance from desiredAspectRatio
-        // not necessarily most optimal code to do this, but easy to follow (hopefully)
         var bestLayout: (rowCount: Int, columnCount: Int) = (1, itemCount)
         var smallestVariance: Double?
         let sizeAspectRatio = abs(Double(size.width/size.height))
